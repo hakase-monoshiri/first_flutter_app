4f38a8e38cf3c40bb5dfa97b16c04e038056b155
Eben Eleazer <ebenaeleazer@outlook.com>
2022-06-18 12:45:27 -0400

    add heart icons to list tiles

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +52,19 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
+
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
+          ),
+          trailing: Icon(
+            // NEW from here ...
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
           ),
         );
         ;
